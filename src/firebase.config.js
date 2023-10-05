31360345bda4791e0ca6c4e4c080f8c8df10ae39
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2023-10-05 22:15:13 +0600

    FireBase Secure

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMSTvp1IE3G2FU5yblaEkyQHJ8CL5eR7M",
-  authDomain: "dragon-news-c7596.firebaseapp.com",
-  projectId: "dragon-news-c7596",
-  storageBucket: "dragon-news-c7596.appspot.com",
-  messagingSenderId: "491457048803",
-  appId: "1:491457048803:web:31bfa57f10396bc72c55d6"
+  apiKey: import.meta.env.VITE_APIKEY ,
+  authDomain: import.meta.env.VITE_AUTHDOMAI ,
+  projectId: import.meta.env.VITE_PROJECTI,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKE,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERI ,
+  appId: import.meta.env.VITE_APPI,
 };
 
 // Initialize Firebase
